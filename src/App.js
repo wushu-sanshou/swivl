021f0afc6b4a4e7ca9ab1b04bad3c05a89436469
wushu-sanshou <wushu-sanshou@alterpost.org>
2018-09-27 10:06:21 +0300

    added dynamic import

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
-import UsersTable from './components/usersTable';
 import { Route } from 'react-router-dom';
-import SingleUser from './components/singleUser';
+import Loadable from 'react-loadable';
 
+
+const UsersTable = () => Loadable({
+  loader: () => import('./components/usersTable'),
+  loading: () => <div>Loading...</div> 
+})
+
+const SingleUser = () => Loadable({
+  loader: () => import('./components/singleUser'),
+  loading: () => <div>Loading...</div> 
+})
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route exact path="/" component={ UsersTable }/>
-        <Route exact path="/user/:username" component={SingleUser}/>
+        <Route exact path="/" component={ UsersTable() }/>
+        <Route exact path="/user/:username" component={SingleUser()}/>
       </div>
     );
   }
